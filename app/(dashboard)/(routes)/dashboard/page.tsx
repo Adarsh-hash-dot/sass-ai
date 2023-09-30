36f221d8f9c4165f7190cd2ac6a987d526cb3887
Adarsh Sahu <adarshsahu1996@gmail.com>
2023-09-30 12:07:37 +0530

    feat: added basic structure for project

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import { Button } from '@/components/ui/button'
+
+const dashboard = () => {
+  return (
+<p className='text-6xl text-green-500'>Hello Sass Dashboard  <Button>Click Me</Button></p>
+  )
+}
+
+export default dashboard
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,8 @@
+import Image from 'next/image'
+import { Button } from '@/components/ui/button'
+
+export default function Home() {
+  return (
+<p className='text-6xl text-green-500'>Hello Sass App  <Button>Click Me</Button></p>
+  )
+}
